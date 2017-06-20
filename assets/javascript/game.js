d15591c199fe629b4de646404566d46bc308a70e
cxwang001 <william@Williams-MBP.attlocal.net>
2017-06-19 17:22:02 -0700

    CrystalsCollector Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,20 +19,20 @@ var emerald_random=0;
 function computerReset() {
   	//
   	random = Math.floor(Math.random() * (120 -19) + 19);
-  	console.log('random', random);
+  	// console.log('random', random);
 
 	ruby_random = Math.floor(Math.random() * (12 -1) + 1);
-	console.log('random1', ruby_random);
+	// console.log('random1', ruby_random);
 			
 	sapphire_random = Math.floor(Math.random() * (12 -1) + 1);
-	console.log('random2', sapphire_random);
+	// console.log('random2', sapphire_random);
 				
 	topaz_random = Math.floor(Math.random() * (12 -1) + 1);
-	console.log('random3', topaz_random);
+	// console.log('random3', topaz_random);
 
 	emerald_random = Math.floor(Math.random() * (12 -1) + 1);
-	console.log('random4', emerald_random);	
-       // ... we generate a random number
+	// console.log('random4', emerald_random);	
+      
    	
 	$("#randomNumber").text(random);  
 
@@ -40,39 +40,43 @@ function computerReset() {
 	$("#score").html('0');
 	
 };
-
+//run the initial setup function
 computerReset();
 //The 4 crystals is waiting for click
 $("img").click(function(){
 	// console.log($(this));
 
-
+//if this has class ruby, add the number to result
 if ($(this).hasClass('ruby')) {
       result += ruby_random;
+      // console.log(this);
       // console.log(result);
   }else if ($(this).hasClass('sapphire')) {
       result += sapphire_random;
+      // console.log(this);
   }else if ($(this).hasClass('topaz')) {
       result += topaz_random;
+      // console.log(this);
   }else if ($(this).hasClass('emerald')) {
       result += emerald_random;
+      // console.log(this);
   }
-  console.log("i am here",result);
+  // console.log("i am here",result);
   $("#score").html(result);
-
+//If score matches, run intial setup again.
 if (result === random) 
-{
+	{
 	wins++;
 	$("#win").html("Wins: " + wins);
-	// result = 0;
-	// computerGenerate();
 	computerReset();
-} else if (result > random) 
-{
+//else if result greater, run initial seutp again.
+
+	} else if (result > random) 
+	{
 	looses++;
-	$("#loss").html("Loosses: "+ looses);
+	$("#loss").html("Losses: "+ looses);
 	computerReset();
-};
+	};
 
 
 });
